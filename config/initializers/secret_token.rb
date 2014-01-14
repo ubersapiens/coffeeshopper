4daf542decba26d9c52c1b0b7fc4a44d0d02ac4c
JFP3 <jfpardini@gmail.com>
2014-01-14 19:19:43 +0100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coffeeshopper::Application.config.secret_key_base = 'cdc9d9a59a7186ef4b87609407a07f90654d362507d1a1e423cc1dfc40f8c21de980e4ec9ec786228e6777ecef8bd4ee9c62687c4a0fdc9c4ee3eaf3904adf93'
